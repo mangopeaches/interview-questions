47145c5298a03d938db95efd90c8fba129511737
Tom Breese <tom.breese@posh.tech>
2024-10-21 17:09:19 -0400

    initial commit

---
+++ src/controllers/apiController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+
+// Mock data that API would return
+const mockData = {
+    message: 'This is some data from the API',
+};
+
+export const apiController = {
+    getData: (req: Request, res: Response) => {
+        res.json(mockData);
+    },
+};
